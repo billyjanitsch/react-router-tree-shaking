ad29511087024a12efa6beccba970a81f9933098
Billy Janitsch <billy@kensho.com>
2019-03-10 23:10:00 -0400

    Commit, prettify, and avoid mangling output

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = {
   mode: 'production',
+  optimization: {
+    minimizer: [new TerserPlugin({terserOptions: {mangle: false}})],
+  },
 }
